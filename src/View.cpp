131d04226b36ef1e1665ce0b7b197b0448c2b02d
Izmar Verhage <izmaelverhage@gmail.com>
2024-11-13 22:52:14 +0100

    Allow area to be specified for includes

--- src/View.cpp
+++ src/View.cpp
@@ -12,25 +12,28 @@ using json = nlohmann::json;
 
 static void from_json(const json& j, node& n)
 {
-    if (j.contains("name"))             j.at("name").get_to(n.name);
-    if (j.contains("type"))             j.at("type").get_to(n.node_type);
-    if (j.contains("svg"))              j.at("svg").get_to(n.svg);
-    if (j.contains("children"))         j.at("children").get_to(n.children);
-    if (j.contains("margin"))           j.at("margin").get_to(n.margin); else n.margin = 0.f;
-    if (j.contains("label"))            j.at("label").get_to(n.label);
-    if (j.contains("label_style"))      j.at("label_style").get_to(n.label_style);
-    if (j.contains("direction"))        j.at("direction").get_to(n.direction);
-    if (j.contains("flex_grow"))        j.at("flex_grow").get_to(n.flex_grow); else n.flex_grow = 0.f;
-    if (j.contains("align_items"))      j.at("align_items").get_to(n.align_items);
-    if (j.contains("row_fractions"))    j.at("row_fractions").get_to(n.row_fractions);
-    if (j.contains("column_fractions")) j.at("column_fractions").get_to(n.column_fractions);
-    if (j.contains("area"))             j.at("area").get_to(n.area);
-
     if (j.contains("include"))
     {
         std::ifstream jsonFile("/Users/izmar/projects/VMPC2000XL/vector UI/views/" + j.at("include").get<std::string>() + ".json");
         json data = json::parse(jsonFile);
         n = data.template get<node>();
+        if (j.contains("area")) j.at("area").get_to(n.area);
+    }
+    else
+    {
+        if (j.contains("name"))             j.at("name").get_to(n.name);
+        if (j.contains("type"))             j.at("type").get_to(n.node_type);
+        if (j.contains("svg"))              j.at("svg").get_to(n.svg);
+        if (j.contains("children"))         j.at("children").get_to(n.children);
+        if (j.contains("margin"))           j.at("margin").get_to(n.margin); else n.margin = 0.f;
+        if (j.contains("label"))            j.at("label").get_to(n.label);
+        if (j.contains("label_style"))      j.at("label_style").get_to(n.label_style);
+        if (j.contains("direction"))        j.at("direction").get_to(n.direction);
+        if (j.contains("flex_grow"))        j.at("flex_grow").get_to(n.flex_grow); else n.flex_grow = 0.f;
+        if (j.contains("align_items"))      j.at("align_items").get_to(n.align_items);
+        if (j.contains("row_fractions"))    j.at("row_fractions").get_to(n.row_fractions);
+        if (j.contains("column_fractions")) j.at("column_fractions").get_to(n.column_fractions);
+        if (j.contains("area"))             j.at("area").get_to(n.area);
     }
 
     printf("=== Deserialized node ===\n");
