7b8317a41243bc2e057b0a1adb536e008068943b
Izmar Verhage <izmaelverhage@gmail.com>
2024-11-17 14:02:21 +0100

    Improve label text colour

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "Constants.hpp"
 
-juce::Colour Constants::labelColour = juce::Colour::fromRGB(70, 70, 70);
+juce::Colour Constants::labelColour = juce::Colour::fromRGB(90, 90, 90);
 float Constants::BASE_FONT_SIZE = 5.0f;
 float Constants::LINE_SIZE = BASE_FONT_SIZE * 0.3f;
 
