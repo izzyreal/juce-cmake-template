15e67e2cc80723067d5f4dc6789ac8f2f15a72c9
Izmar Verhage <izmaelverhage@gmail.com>
2024-11-26 19:04:07 +0100

    Make shadow lighter globally

--- src/Shadow.hpp
+++ src/Shadow.hpp
@@ -18,7 +18,7 @@ class Shadow : public juce::Component {
             auto radius = scale * shadowSize;
             juce::Point<float> offset = { 1.f * scale * shadowSize, 0.1f * scale * shadowSize };
             auto spread = 0.f;
-            auto color = juce::Colours::black.withAlpha(0.6f);
+            auto color = juce::Colours::black.withAlpha(0.4f);
             melatonin::DropShadow shadow = { color, radius, offset, spread };
             auto path = getPath();
             juce::AffineTransform transform;
