7db54f26d07bb4f575f7319f370c1c2fb42e643b
Izmar Verhage <izmaelverhage@gmail.com>
2024-11-28 22:01:09 +0100

    Make LCD colours nice and add 3D cube

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -8,9 +8,9 @@ juce::Colour Constants::darkLabelColour = labelColour.darker();
 juce::Colour Constants::greyFacePaintColour = juce::Colour::fromRGB(139, 151, 163);
 juce::Colour Constants::betweenChassisAndLabelColour = chassisColour.darker(0.2f);
 
-juce::Colour Constants::lcdOn = juce::Colour::fromRGB(0, 0, 127);
-juce::Colour Constants::lcdOnLight = Constants::lcdOn.brighter();
-juce::Colour Constants::lcdOff = juce::Colour::fromRGB(0, 127, 200);
+juce::Colour Constants::lcdOn = juce::Colour::fromRGB(86, 61, 145);
+juce::Colour Constants::lcdOnLight = Constants::lcdOn.brighter(0.8f);
+juce::Colour Constants::lcdOff = juce::Colour::fromRGB(170, 248, 218);
 
 float Constants::BASE_FONT_SIZE = 5.0f;
 float Constants::LINE_SIZE = BASE_FONT_SIZE * 0.1f;
